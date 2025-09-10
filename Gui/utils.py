d0a247c870b95a9ce6e6fe5a155e432bc047d190
wildbearbaux <alfredvs14@gmail.com>
2025-09-10 11:43:39 -0600

    xd

--- Gui/utils.py
+++ Gui/utils.py
@@ -1,13 +1,22 @@
-from config import *
+from config import CELLS_ROW_SIZE, CELLS_COLUMN_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT
 
-def get_pixels_from_coordinates(row, col):
-    """
-    :param row: The number of col
-    :param col: The number of row
-    :return: tuple (x, y) in pixels from screen in the center of the cell
-    """
+CELL_W = WINDOW_WIDTH  // CELLS_COLUMN_SIZE
+CELL_H = WINDOW_HEIGHT // CELLS_ROW_SIZE
 
-    if 0 < row <= CELLS_ROW_SIZE:
-        if 0 < col <= CELLS_COLUMN_SIZE:
-            return (COLS_PADDING * col), (ROWS_PADDING * row)
+def get_pixels_from_coordinates(row, col, center=True, one_based=True):
+    # valida indices
+    if one_based:
+        if not (1 <= row <= CELLS_ROW_SIZE and 1 <= col <= CELLS_COLUMN_SIZE):
+            return None
+        row -= 1; col -= 1
+    else:
+        if not (0 <= row < CELLS_ROW_SIZE and 0 <= col < CELLS_COLUMN_SIZE):
+            return None
+
+    x = col * CELL_W
+    y = row * CELL_H
+    if center:
+        x += CELL_W // 2
+        y += CELL_H // 2
+    return x, y
 
